4336dfc16130cfa968267d99792e3f2d771ba2da
Tumi <23521181@gm.uit.edu.vn>
2025-11-17 11:20:27 +0700

    thêm routes cho Category

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -10,18 +10,12 @@ import {
 
 const router = express.Router();
 
+// tất cả route dưới đây đều yêu cầu đăng nhập
 router.use(requireAuth);
 
-// GET /api/v1/categories
 router.get("/", getCategories);
-
-// POST /api/v1/categories
 router.post("/", createCategory);
-
-// PUT /api/v1/categories/:id
 router.put("/:id", updateCategory);
-
-// DELETE /api/v1/categories/:id
 router.delete("/:id", deleteCategory);
 
 export default router;
